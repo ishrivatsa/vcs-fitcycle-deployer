9717ce08d8a694d52ff0804c9a65c0d4578a831f
ishrivatsa <ushrivatsa@gmail.com>
2018-11-06 12:21:07 -0800

    Add terraform support for RDS

---
+++ fitcycle_terraform/variables.tf
@@ -0,0 +1,34 @@
+# Variables for accepting Access Key and Secret key for AWS
+# Default region is set to us-east-1
+
+variable "use_rds_database" {}
+variable "multi_az_rds" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {
+    default = "us-east-1"
+}
+variable "images" {
+  type = "map"
+  default = {
+    web="ami-0424ce05e6eac4d44"
+    mgmt="ami-0d09699c3879b47a6"
+    dblb="ami-0c287d8bb736b0dc4"
+    db="ami-03442710b971503b5"
+    app="ami-0c5a97dcec802ce81"
+    api="ami-04aba6a14439a24d2"
+   }
+}
+
+
+variable "aws_vpc_name" {}
+variable "aws_vpc_cidr" {}
+variable "aws_ssh_key_name" {}
+variable "aws_public_ssh_key" {}
+
+variable "product" {}
+variable "team" {}
+variable "owner" {}
+variable "environment" {}
+variable "organization" {}
+variable "costcenter" {}
