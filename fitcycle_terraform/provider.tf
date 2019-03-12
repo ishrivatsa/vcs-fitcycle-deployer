1fd934f31afb99594f8ee7933f3864e0f6628425
ishrivatsa <ushrivatsa@gmail.com>
2019-03-12 14:56:48 -0700

    Add S3 as remote backend

--- fitcycle_terraform/provider.tf
+++ fitcycle_terraform/provider.tf
@@ -5,3 +5,17 @@ provider "aws" {
   secret_key = "${var.option_2_aws_secret_key}"
   region     = "${var.region}"
 }
+
+# This allows terraform to backup the *.tfstate file to AWS s3 bucket. Uncomment or Remove the lines to disable remote backup and use local state (Not Recommended)
+# terraform {
+#   backend "s3" {
+#     bucket = "mybucket"
+#     key    = "path/to/my/key/some.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+# Here it is initialized with empty parameters. Other params can be passed at "terraform init --backend-config="bucket=mybucket" --backend-config="key=path/to/my/key/some.tfstate" --backend-config="region=us-east-1""
+
+terraform {
+   backend "s3" {}
+}
