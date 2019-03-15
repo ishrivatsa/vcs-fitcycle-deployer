9ac4e95b304d9ee744452949005d1a62e2e6db4d
Shrivatsa <ushrivatsa@gmail.com>
2019-03-15 15:50:17 -0700

    Update variables.tf

--- fitcycle_terraform/variables.tf
+++ fitcycle_terraform/variables.tf
@@ -21,12 +21,14 @@ variable "images" {
 
 variable "option_3_aws_vpc_name" {}
 variable "option_4_aws_vpc_cidr" {}
-variable "option_5_aws_ssh_key_name" {}
-variable "option_6_aws_public_ssh_key" {}
+variable "option_5_aws_admin_ssh_key_name" {}
+variable "option_6_aws_admin_public_ssh_key" {}
+variable "option_7_aws_dev_ssh_key_name" {}
+variable "option_8_aws_dev_public_ssh_key" {}
 
-variable "option_7_use_rds_database" {}
-variable "option_8_aws_rds_identifier" {}
-variable "option_9_multi_az_rds" {}
+variable "option_9_use_rds_database" {}
+variable "option_10_aws_rds_identifier" {}
+variable "option_11_multi_az_rds" {}
 
 variable "product" {}
 variable "team" {}
